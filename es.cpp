e65a78530316ebd54bdb64064d81c7913fb860e5
fedeebinii <binifederico06@gmail.com>
2024-10-27 11:31:35 +0100

    Aggiunto e modificato file es.cpp

---
+++ es.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+//ciao mondo
+using namespace std;
+
+double distanza (double X1 , double Y1, double X2, double Y2){
+    double d = sqrt ((X1-X2)*(X1-X2)+(Y1-Y2)*(Y1-Y2));
+    return d;
+
+}
+
+
+int main (void){
+    
+    double X1,X2,Y1,Y2,X3,Y3;
+    
+    cout<<"Coordinata primo punto asse x : "; cin>>X1;
+    cout<<"Coordinata primo punto asse y : "; cin>>Y1;
+    cout<<"Coordinata secondo punto asse x : "; cin>>X2;
+    cout<<"Coordinata secondo punto asse x : "; cin>>Y2;
+
+    double distanza_punti = distanza(X1,X2,Y1,Y2);
+
+    cout<<"La distanza fra i due punti è:"<<distanza_punti<<endl;
+
+    return 1;
+}
